e5301816500e482c353eaf257605b56944754163
krlos <djkrlosmix1@gmail.com>
2018-10-10 17:06:20 -0500

    ejercicios get

--- api(Iris)/main.go
+++ api(Iris)/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/kataras/iris"
 )
 
@@ -20,8 +23,27 @@ func main() {
 		ctx.SendFile("./files/screen.png", "archivoDes.png") // primer parametro el archivo a descargar, el segundo el nombre y extension del archivo cuando se descargue
 	})
 
-	app.Get("/param", func() {
+	app.Get("/param/{params:int}", func(ctx iris.Context) { // se puede especificar el tipo de data a recibir
+		//paramt := ctx.Params().Get("params")  -> para obtener la data que se manda al api, es con Get("param") y usando entre comillas "", string
+		paramt, _ := ctx.Params().GetInt("params") // para ibtener el tipo de dato int
+		fmt.Println(paramt)
+	})
 
+	app.Get("/user/{id regexp([0-9][A-Z])}", func(ctx iris.Context) { // para definir los tipos de caracteres permitidos y la cantidad permitida
+		id := ctx.Params().Get("id")
+		fmt.Println(id)
+	})
+
+	app.Macros().Int.RegisterFunc("isPair", func() func(string) bool {
+		return func(paramValue string) bool {
+			eval, _ := strconv.Atoi(paramValue)
+			return eval%2 == 0
+		}
+	})
+
+	app.Get("/id/{id:int isPair()}", func(ctx iris.Context) {
+		id, _ := ctx.Params().GetInt("id")
+		fmt.Println(id)
 	})
 
 	app.Run(iris.Addr(":8080"))
